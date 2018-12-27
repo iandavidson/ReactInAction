1188ca94b879e31668b0df6c54774eb210907859
Ian Davidson <ianmattdavidson@gmail.com>
2018-12-27 14:55:32 -0800

    small updates upto 2.2.4
    renamed index.js => index-HelloWorld.js
    added index.js {containing a react class}

---
+++ chapter2/section2.2.3/src/index-HelloWorld.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+import PropTypes from "prop-types";
+
+const root = document.getElementById("root");
+const virtualRoot = React.createElement(
+  "div",
+  {},
+  React.createElement(
+    "h1",
+    {},
+    "Hello, world!",
+    React.createElement(
+      "a",
+      { href: "mailto:mark@ifelse.io" },
+      React.createElement("h1", {}, "React In Action"),
+      React.createElement("em", {}, "...wow italics")
+    )
+  )
+);
+
+render(virtualRoot, root);
--- chapter2/section2.2.3/src/index.js
+++ chapter2/section2.2.3/src/index.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
-const root = document.getElementById("root");
-const virtualRoot = React.createElement(
-  "div",
-  {},
-  React.createElement(
-    "h1",
-    {},
-    "Hello, world!",
-    React.createElement(
-      "a",
-      { href: "mailto:mark@ifelse.io" },
-      React.createElement("h1", {}, "React In Action"),
-      React.createElement("em", {}, "...wow italics")
-    )
-  )
-);
+import PropTypes from "prop-types";
 
-render(virtualRoot, root);
+const node = document.getElementById("root");
+class Post extends Component {
+  render() {
+    return React.createElement(
+      "div",
+      {
+        className: "post"
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "postAuthor",
+          id: this.props.id
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "postBody"
+          },
+          this.props.content
+        )
+      )
+    );
+  }
+}
+
+Post.propTypes = {
+  user: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
+};
+
+const App = React.createElement(Post, {
+  id: 1,
+  content: " said: this is a post!",
+  user: "scott"
+});
+
+render(App, node);
