948819342305921d6a60d9bf5d21967c04da1524
Ian Davidson <ianmattdavidson@gmail.com>
2018-12-28 11:08:26 -0800

    small updates to index.js
    ...

--- chapter2/section2.2.3/src/index.js
+++ chapter2/section2.2.3/src/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import PropTypes from "prop-types";
-
 const node = document.getElementById("root");
 class Post extends Component {
   render() {
@@ -32,13 +31,62 @@ class Post extends Component {
 Post.propTypes = {
   user: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired
+  id: PropTypes.number.isRequired
 };
 
-const App = React.createElement(Post, {
-  id: 1,
-  content: " said: this is a post!",
-  user: "scott"
-});
+class Comment extends Component {
+  render() {
+    console.log("lameeee");
+    return React.createElement(
+      "div",
+      {
+        className: "comment"
+      },
+      React.createElement(
+        "h2",
+        {
+          className: "commentAuthor"
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className: "commentContent"
+          },
+          this.props.content
+        )
+      )
+    );
+  }
+}
+
+Comment.propTypes = {
+  id: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired
+};
+
+const comment = React.createElement(
+  Comment, 
+  {
+    id: 3,
+    content: "starting to get irritated",
+    user: "ur mom! zing"
+})
+
+const App = React.createElement(
+  Post,
+  {
+    id: 1,
+    content: " said: this is a post!",
+    user: "scott"
+  },
+  comment
+  // React.createElement(Comment, {
+  //   id: 2,
+  //   content: " commented: wow! how cool!",
+  //   user: "bob"
+  // })
+);
 
 render(App, node);
